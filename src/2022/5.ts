c028567f85c91d396bfe3c64e0dfb90adaabd9a7
DTrombett <maxtromb.dt@gmail.com>
2022-12-05 12:26:04 +0100

    Day 5

---
+++ src/2022/5.ts
@@ -0,0 +1,24 @@
+import { readFileSync } from "node:fs";
+
+const input = readFileSync("./inputs/2022/5", { encoding: "utf-8" });
+///////////////////////////////////////////////////////////////////////////////
+
+const inputs = input.split(/\s\s(?:[1-9]\s+)+/);
+const crates: string[][] = [];
+
+for (const line of inputs[0].split("\n")) {
+	const letters = line.match(/.{1,4}/g);
+
+	if (!letters) continue;
+	for (let i = 0; i < letters.length; i++)
+		if (letters[i][1] !== " ") (crates[i] ??= []).push(letters[i][1]);
+}
+for (const instruction of inputs[1].split("\n")) {
+	const instructions = instruction.match(/\d+/g)?.map((d) => Number(d));
+
+	if (!instructions) continue;
+	crates[instructions[2] - 1].unshift(
+		...crates[instructions[1] - 1].splice(0, instructions[0])
+	);
+}
+console.log(crates.map((c) => c[0]).join(""));
