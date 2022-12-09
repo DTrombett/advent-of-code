0ca31a0af0d7b0ef7debf25c6e99617eee756051
DTrombett <maxtromb.dt@gmail.com>
2022-12-09 19:11:46 +0100

    Day 9 - Part 2 (failed)

--- src/2022/9.ts
+++ src/2022/9.ts
@@ -1,34 +1,40 @@
+/* eslint-disable sort-vars */
 import { getInput, time } from "../utils";
 
 const input = getInput("2022/9");
 const start = performance.now();
 
+// NOTE: This actually gives a wrong result
 const touched = new Set(["0-0"]);
-const h = [0, 0],
-	t = [0, 0];
-const abs = (n: number) => (n < 0 ? -n : n);
+const knotsCount = 10,
+	knots = Array.from({ length: knotsCount + 1 }, () => [0, 0]);
+const [head] = knots;
 
 for (const line of input.split("\n")) {
-	const [d, a] = line.split(" ");
-	const n = Number(a);
+	const [d, _n] = line.split(" ");
+	const n = Number(_n);
 
 	for (let j = 0; j < n; j++) {
-		if (d === "L") h[0]--;
-		else if (d === "R") h[0]++;
-		else if (d === "U") h[1]++;
-		else if (d === "D") h[1]--;
+		if (d === "L") head[0]--;
+		else if (d === "R") head[0]++;
+		else if (d === "U") head[1]++;
+		else if (d === "D") head[1]--;
 		else break;
-		const difX = h[0] - t[0],
-			difY = h[1] - t[1];
+		for (let k = 1; k < knots.length; k++) {
+			const prevKnot = knots[k - 1],
+				difX = prevKnot[0] - knots[k][0],
+				difY = prevKnot[1] - knots[k][1];
 
-		if (abs(difX) === 2) {
-			t[0] += difX === 2 ? 1 : -1;
-			t[1] += difY;
-		} else if (abs(difY) === 2) {
-			t[1] += difY === 2 ? 1 : -1;
-			t[0] += difX;
+			if (Math.abs(difX) > 1) {
+				knots[k][0] += difX > 1 ? 1 : -1;
+				knots[k][1] += difY > 0 ? 1 : -1;
+			}
+			if (Math.abs(difY) > 1) {
+				knots[k][1] += difY > 1 ? 1 : -1;
+				knots[k][0] += difX > 0 ? 1 : -1;
+			}
 		}
-		touched.add(t.join("-"));
+		touched.add(knots.at(-1)!.join("-"));
 	}
 }
 const { size } = touched;
