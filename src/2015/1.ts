86adb2420894b6d5d28363a3d89fdf3af813fe0f
DTrombett <maxtromb.dt@gmail.com>
2022-12-10 18:33:35 +0100

    Day 1 (2015) - Part 2

--- src/2015/1.ts
+++ src/2015/1.ts
@@ -3,8 +3,14 @@ import { getInput, time } from "../utils";
 const input = getInput("2015/1");
 const start = performance.now();
 
-const result = input.match(/\(/g)!.length * 2 - input.length;
+const values: Record<string, number> = {
+	"(": 1,
+	")": -1,
+};
+let floor = 0;
 
+const result =
+	input.split("").findIndex((v) => (floor += values[v]) === -1) + 1;
 const end = performance.now();
 
 console.log(result, time(start, end));
