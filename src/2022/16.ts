32ccf8198a44c9ae6a49ba55bc78de48594cf9d1
DTrombett <maxtromb.dt@gmail.com>
2022-12-17 20:00:22 +0100

    Day 16 (2022) - Part 1 (Failed)

---
+++ src/2022/16.ts
@@ -0,0 +1,46 @@
+import { getInput, time } from "../utils";
+
+type Valve = {
+	name: string;
+	opened: boolean;
+	pressure: number;
+	tunnels: Valve[];
+	tunnelsString: string[];
+};
+const input = getInput("2022/16");
+const start = performance.now();
+
+const valves: Valve[] = input.split("\n").map((line) => {
+	const [name, pressure, ...tunnelsString] = line.match(/\d+|[A-Z]{2}/g)!;
+
+	return {
+		name,
+		opened: false,
+		pressure: Number(pressure),
+		tunnels: [],
+		tunnelsString,
+	};
+});
+let [current] = valves;
+let increasing = 0;
+let pressure = 0;
+
+for (const valve of valves)
+	valve.tunnels = valve.tunnelsString
+		.map((v) => valves.find(({ name }) => name === v)!)
+		.sort((a, b) => b.pressure - a.pressure);
+for (let min = 1; min <= 30; min++) {
+	pressure += increasing;
+	if (current.opened || !current.pressure)
+		current =
+			current.tunnels.find((t) => !t.opened) ??
+			current.tunnels.find((t) => t.tunnels.find((n) => !n.opened)) ??
+			current.tunnels[0];
+	else {
+		increasing += current.pressure;
+		current.opened = true;
+	}
+}
+const end = performance.now();
+
+console.log(pressure, time(start, end));
