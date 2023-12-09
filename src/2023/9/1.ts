0377c3652aed6781b777b2ae0c4bd40568a07c40
DTrombett <maxtromb.dt@gmail.com>
2023-12-09 13:07:26 +0100

    Day 9 (2023)

---
+++ src/2023/9/1.ts
@@ -0,0 +1,21 @@
+const firstPart = (input: string) =>
+	input.split("\n").reduce((sum, history) => {
+		const sequence = history.split(" ") as unknown as number[];
+		let needContinue: boolean;
+
+		sum += parseInt(sequence.at(-1) as unknown as string);
+		do {
+			needContinue = false;
+			for (let j = 1; j < sequence.length; j++) {
+				const lastIndex = j - 1;
+
+				sequence[lastIndex] = sequence[j] - sequence[lastIndex];
+				needContinue ||= sequence[lastIndex] !== 0;
+			}
+			sequence.length--;
+			sum += sequence.at(-1)!;
+		} while (needContinue);
+		return sum;
+	}, 0);
+
+export default firstPart;
---
+++ src/2023/9/2.ts
@@ -0,0 +1,23 @@
+const firstPart = (input: string) =>
+	input.split("\n").reduce((sum, history) => {
+		const sequence = history.split(" ") as unknown as number[];
+		let needContinue: boolean;
+		let first = parseInt(sequence[0] as unknown as string);
+		let add = false as boolean;
+
+		do {
+			needContinue = false;
+			for (let j = 1; j < sequence.length; j++) {
+				const lastIndex = j - 1;
+
+				sequence[lastIndex] = sequence[j] - sequence[lastIndex];
+				needContinue ||= sequence[lastIndex] !== 0;
+			}
+			sequence.length--;
+			if ((add = !add)) first -= sequence[0];
+			else first += sequence[0];
+		} while (needContinue);
+		return (sum += first);
+	}, 0);
+
+export default firstPart;
