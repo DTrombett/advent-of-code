ffaa61f02c7f617ca7b19dff2c0bc2d9a787ec32
DTrombett <d@trombett.org>
2024-12-07 13:34:20 +0100

    2024, day 7

---
+++ src/2024/7/1.ts
@@ -0,0 +1,27 @@
+const regexp = /(\d+): ((?:\d+ ?)+)/gu;
+
+const resolveResult = (numbers: string[], t: number): boolean => {
+	numbers = numbers.slice(0);
+	let n = numbers.pop();
+
+	for (; n && numbers.length && t > 0; n = numbers.pop()) {
+		const num = Number(n);
+
+		if (t % num === 0 && resolveResult(numbers, t / num)) return true;
+		t -= num;
+	}
+	return t === Number(n);
+};
+
+const secondPart = (input: string) => {
+	let total = 0;
+
+	for (const [, r, n] of input.matchAll(regexp)) {
+		const result = Number(r);
+
+		if (resolveResult(n!.split(" "), result)) total += result;
+	}
+	return total;
+};
+
+export default secondPart;
