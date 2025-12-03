2118a5dd713c559716a43c8904dcb6ba69b0d9a9
DTrombett <d@trombett.org>
2025-12-03 16:32:48 +0100

    2025, day 3

---
+++ src/2025/3/2.ts
@@ -0,0 +1,25 @@
+const BATT = 12;
+
+export default (input: string) =>
+	input.split("\n").reduce((acc, line) => {
+		const batteries = line.split("").map(Number);
+		const sorted = batteries.toSorted((a, b) => b - a);
+		let minL = 0;
+
+		for (let j = BATT; j > 0; ) {
+			const maxL = batteries.length - j;
+			let k = 0,
+				l = 0;
+
+			for (; k < sorted.length; k++) {
+				if (sorted[k] === 0) continue;
+				l = minL;
+				while (l < maxL && batteries[l] !== sorted[k]) l++;
+				if (batteries[l] === sorted[k]) break;
+			}
+			minL = l + 1;
+			acc += sorted[k]! * 10 ** --j;
+			sorted[k] = 0;
+		}
+		return acc;
+	}, 0);
